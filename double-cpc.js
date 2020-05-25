013c86b806f0c8c6755b465007d5dac2dbc8a079
Candice Marie Martinez <39352383+CMarieMartinez@users.noreply.github.com>
2020-05-24 23:34:11 -0400

    Create double-cpc.js

---
+++ double-cpc.js
@@ -0,0 +1,31 @@
+function main() {
+    function increaseKeywordBid() {
+        var keywordIterator = AdsApp.keywords() // Get keywords from Google Ads api
+            .withCondition("CampaignName = 'Awesome Search Campaign'")
+            .forDateRange('LAST_MONTH')
+            .orderBy('Impression DESC')
+            .get();
+
+        while (keywordIterator.hasNext()) {
+            var keyword = keywordIterator.next();
+            var metrics = keywords.getStatsFor('LAST_MONTH');
+            var CTR = metrics.getCtr() * 100;   // CTR is a decimal so convert the decimal by 100 for easier reading
+            var newBid = metrics.getAverageCpc() * 2;  // Creating doubled CPC
+
+            if (keyword.isEnabled()) {
+                if ((CTR > 6) && (metrics.getConversions() > 0)) { // Getting high performing keywords for report
+                    Logger.log(keyword.getText() + ':');
+                    Logger.log("CTR: " + CTR.toFixed(2));
+                    Logger.log("Impressions: " + metrics.getImpressions());
+                    Logger.log("Clicks: " + metrics.getClicks());
+                    Logger.log("Conversions: " + metrics.getConversions());
+
+                    if (metrics.getImpressions() < 100) { 
+                        keyword.bidding().setCpc(newBid); // Set new CPC
+                    }
+                }
+            }
+        }
+    }
+    increaseKeywordBid();
+}
